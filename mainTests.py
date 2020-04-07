86b6a46fa274b12c05cd3723a6a9ee1dd94d835f
Matt <lucidtalents@yahoo.com>
2020-04-07 14:41:27 -0500

    finished writing tests. Now need to develop code to make tests succeed.

--- mainTests.py
+++ mainTests.py
@@ -1,7 +1,8 @@
 import unittest
-
+import random
 
 class TestInput(unittest.TestCase):
+
     def setUp(self):
         # Assume meeting output, and then test characteristics using algos, but I know expected output (by manual assert)
         try:
@@ -17,34 +18,90 @@ class TestInput(unittest.TestCase):
         info = self.algoInput.info  # from JSON File
         # Get all keys from json
         # check if all keys present -> indicates form considered correct information.
-        #!= None
-        info["tasks"]
-        info["people"]
+
+        self.assertIsNotNone(info["tasks"])
+        self.assertIsNotNone(info["people"])
 
         firstPerson = info["people"][0]
         firstTask = info["tasks"][0]
-        firstPerson["uniqueID"]
-        firstPerson["capacity"]
-        firstPerson["taskAvail"]
-        firstPerson["tags"]
 
-        firstTask["uniqueID"]
-        firstTask["estimate"]
-        firstTask["devAvail"]
-        firstTask["tags"]
+        # Could combine these into "person fields failed..." or something more generic
+        try:
+            a = firstPerson["uniqueID"]
+        except NameError:
+            print("person uniqueID field failed")
+            self.assertTrue(False)
 
-        firstTask["child"]
-        firstTask["parent"]
-        firstTask["dependsOn"]
-        firstTask["dependedBy"]
+        try:
+            a = firstPerson["capacity"]
+        except NameError:
+            print("person capacity field failed")
+            self.assertTrue(False)
+        try:
+            a = firstPerson["taskAvail"]
+        except NameError:
+            print("person taskAvail field failed")
+            self.assertTrue(False)
 
-        self.assertEqual(True, True)
+        try:
+            a = firstPerson["tags"]
+        except NameError:
+            print("person tags field failed")
+            self.assertTrue(False)
+
+        try:
+            a = firstTask["uniqueID"]
+        except NameError:
+            print("task uniqueID field failed")
+            self.assertTrue(False)
+
+        try:
+            a = firstTask["estimate"]
+        except NameError:
+            print("task estimate field failed")
+            self.assertTrue(False)
+
+        try:
+            a = firstTask["devAvail"]
+        except NameError:
+            print("task devAvail field failed")
+            self.assertTrue(False)
+
+        try:
+            a = firstTask["tags"]
+        except NameError:
+            print("tags field failed")
+            self.assertTrue(False)
+
+        try:
+            a = firstTask["child"]
+        except NameError:
+            print("task child field failed")
+            self.assertTrue(False)
+
+        try:
+            a = firstTask["parent"]
+        except NameError:
+            print("task parent field failed")
+            self.assertTrue(False)
+
+        try:
+            a = firstTask["dependsOn"]
+        except NameError:
+            print("task dependsOn field failed")
+            self.assertTrue(False)
+
+        try:
+            a = firstTask["dependedBy"]
+        except NameError:
+            print("task dependedBy field failed")
+            self.assertTrue(False)
 
     def testTaskLinkTraversal(self):
         # - Check if dependency/child tasks can be traversed up/down
         tasks = self.algoInput.tasks
         # value is dictionary of x [key] y. IE: Task 1 dependsOn Task 2
-        expectedOutput = {"dependsOn":1,"dependedBy":2,"child":3,"parent",4}
+        expectedOutput = {"dependsOn":1,"dependedBy":2,"child":3,"parent":4}
         for key,value in expectedOutput:
             expectedOutput[key] = sorted(expectedOutput[key])
 
@@ -67,27 +124,41 @@ class TestInput(unittest.TestCase):
     def testCapacity(self):
         # - Check algorithm points mapping and capacity (does mapping occur correctly? Do we aggregate
         # capacity and determine leftover capacity correctly?)
+        expectedCapacity = 111111
+        expectedTaskEstimate = 11111
+        exepctedDifference = expectedCapacity - expectedTaskEstimate
+
         peopleArr = self.algoInput.people
         # sum up all peopleArr.capacity
+        capacity = 0
+        for person in self.algoInput.people:
+            capacity += person["capacity"]
+
         # sum up all tasks.capacity
-        # determine leftover
-        # should do manually and check against that
-        self.assertEqual(True, True)
+        taskEstimate = 0
+        for task in self.algoInput.tasks:
+            taskEstimate += task["estimate"]
+
+        self.assertEqual(expectedCapacity, capacity)
+        self.assertEqual(expectedTaskEstimate, taskEstimate)
 
     def testDevTaskMatching(self):
         # - Does developer task matching work?
         self.algorithm.matchNecessaries() # object includes available people/matches and such
 
-        # return list of matches
-        # compare with expected list.
-        self.assertEqual(True, True)
+        self.assertTrue(self.algorithm.people["Bob"].assignedTask != None)
+        self.assertTrue(self.algorithm.people["Jill"].assignedTask != None)
+
+        self.algorithm.resetMatches() # reset what we have matched
 
     def testEasySolution(self):
         # - Can we complete a solution (given easy problem?)
 
-        # do random algorithm (perm seed so it is the same every time for the test)
+        random.seed(17)
+        self.algorithm.randomSearch() # find random solution
+
         # check if it meets known constraints. This will be basis for constraint checking a generic solution.
-        self.assertEqual(True, True)
+        self.assertTrue(self.algorithm.checkHardConstraints())
 
     def tearDown(self):
         try:
@@ -99,23 +170,5 @@ class TestInput(unittest.TestCase):
         except NameError:
             print("Teardown failed")
 
-
-'''
-class TestStringMethods(unittest.TestCase):
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-'''
 if __name__ == '__main__':
     unittest.main()
